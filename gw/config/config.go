860fb01c666329732d2adccd3af3f134dbe7cb7d
Justen Walker <justen.walker+github@gmail.com>
2021-03-31 01:19:42 -0400

    refactor!: rename TaskConfig to ProcessConfig
    - document the fields and structures
    - rname some of the RepositoryInfo fields to avoid stutter

--- gw/config/config.go
+++ gw/config/config.go
@@ -14,46 +14,64 @@ type Service struct {
 	client pb.ConfigServiceClient
 }
 
+// NewService creates a new config.Service for getting task configuration
 func NewService(conn grpc.ClientConnInterface) *Service {
 	return &Service{
 		client: pb.NewConfigServiceClient(conn),
 	}
 }
 
-type TaskConfig struct {
-	ProcessID        string
+// ProcessConfig is the configuration of the running process
+type ProcessConfig struct {
+	// ID is the UUID of the process
+	ID string
+	// WorkingDirectory is the current working directory of the process
 	WorkingDirectory string
-	ProjectInfo      ProjectInfo
-	RepositoryInfo   RepositoryInfo
+	// ProjectInfo has the information about the Project in which the process is running (optional)
+	ProjectInfo ProjectInfo
+	// RepositoryInfo has information about to the Repository inside the Project that spawned the process (optional)
+	RepositoryInfo RepositoryInfo
+	// APIConfiguration contains connection details to the Concord API
 	APIConfiguration APIConfiguration
 }
 
+// ProjectInfo contains information about the project in which the process is running
 type ProjectInfo struct {
-	OrgID       string
-	OrgName     string
-	ProjectID   string
+	// OrgID is the UUID of the Organization
+	OrgID string
+	// OrgName is the name of the Organization
+	OrgName string
+	// ProjectID is the UUID of the Project
+	ProjectID string
+	// ProjectName is the name of the Project
 	ProjectName string
 }
 
+// RepositoryInfo contains details about the repository that is configured for the process
 type RepositoryInfo struct {
-	RepoID   string
-	RepoName string
-	RepoURL  string
+	// ID is the UUID of the Repository
+	ID string
+	// Name is the name of the Repository
+	Name string
+	// URL is the URL of the Repository
+	URL string
 }
 
+// APIConfiguration has the api details and credentials for communicating directly with the Concord API
+// as part of the process
 type APIConfiguration struct {
 	BaseURL      string
 	SessionToken string
 }
 
 // Configuration returns the task configuration
-func (c *Service) Configuration(ctx context.Context) (*TaskConfig, error) {
+func (c *Service) Configuration(ctx context.Context) (*ProcessConfig, error) {
 	resp, err := c.client.GetConfiguration(ctx, &pb.ConfigurationRequest{})
 	if err != nil {
 		return nil, err
 	}
-	return &TaskConfig{
-		ProcessID:        resp.ProcessID,
+	return &ProcessConfig{
+		ID:               resp.ProcessID,
 		WorkingDirectory: resp.WorkingDirectory,
 		ProjectInfo: ProjectInfo{
 			OrgID:       resp.ProjectInfo.OrgID,
@@ -62,9 +80,9 @@ func (c *Service) Configuration(ctx context.Context) (*TaskConfig, error) {
 			ProjectName: resp.ProjectInfo.ProjectName,
 		},
 		RepositoryInfo: RepositoryInfo{
-			RepoID:   resp.RepositoryInfo.RepoID,
-			RepoName: resp.RepositoryInfo.RepoName,
-			RepoURL:  resp.RepositoryInfo.RepoURL,
+			ID:   resp.RepositoryInfo.RepoID,
+			Name: resp.RepositoryInfo.RepoName,
+			URL:  resp.RepositoryInfo.RepoURL,
 		},
 		APIConfiguration: APIConfiguration{
 			BaseURL:      resp.ApiConfiguration.BaseURL,
